ea90dcacf4e680ddbfe371aa11651acb90ee64b1
t-nakatani <tomoya.12@icloud.com>
2023-01-13 12:48:50 +0900

    [UPDATE] integrate ja2en translation

--- info_search/views/basic.py
+++ info_search/views/basic.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from functions.search_tweets import get_tweepy_client, get_tweets
 from functions.sentiment_analyze import SentimentAnalysis
-import requests
+from easynmt import EasyNMT
+import requests  # TODO: remove
 
 sa_model = SentimentAnalysis()
+trans_model = EasyNMT('opus-mt')
 
 def home(request):
     """ホームページ"""
@@ -17,13 +19,21 @@ def home(request):
 
 def search_tweets(request, region):
     """地域(ex. 大阪)のボタンを押した際に動作"""
+    if request.method == 'POST':
+        render(request, 'home.html')
     if request.method == 'GET':
         print('=============  search_tweets  ================')
         tweepy_client = get_tweepy_client()
         responses, texts = get_tweets(tweepy_client, region)
         sentiments = [sa_model.get_label(txt) for txt in texts]
         positive_responses = [r for r, s in zip(responses, sentiments) if s == 'ポジティブ']
-        if len(positive_responses) < 6:
-            return render(request, 'home.html', {'responses': positive_responses})
+        positive_texts = [t for t, s in zip(texts, sentiments) if s == 'ポジティブ']
+        translated_pos_texts = [
+            trans_model.translate(text, target_lang='en', max_new_tokens=500) for text in positive_texts
+        ]
+        [print(text) for text in translated_pos_texts]
+        context = {'responses': positive_responses[:6], 'en_texts': translated_pos_texts}
 
-    return render(request, 'home.html', {'responses': positive_responses[:6]})
+        if len(positive_responses) < 6:
+            return render(request, 'home.html', context)
+    return render(request, 'home.html', context)
